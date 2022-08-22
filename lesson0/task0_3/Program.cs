cedfe4032f7fada02efb395aeafe88a1ae6d51f5
VanUliK <vanulik@work.com>
2022-08-22 16:22:02 +0200

    create lesson6

---
+++ lesson0/task0_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int Factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    } 
+    else
+    {
+        return n * Factorial(n-1);
+    }
+}
+System.Console.WriteLine(Factorial(5));
--- lesson1/task3/Program.cs
+++ lesson1/task3/Program.cs
@@ -5,14 +5,23 @@
 Console.WriteLine("Введите номер дня недели: ");
 int i = Convert.ToInt32(Console.ReadLine());
 
-// if (i > 7 || i < 1) Console.WriteLine("Такого дня не существует");
-// if (i == 1) Console.WriteLine("Понедельник");
-// if (i == 2) Console.WriteLine("Вторник");
-// if (i == 3) Console.WriteLine("Среда");
-// if (i == 4) Console.WriteLine("Четверг");
-// if (i == 5) Console.WriteLine("Пятница");
-// if (i == 6) Console.WriteLine("Суббота");
-// if (i == 7) Console.WriteLine("Воскресенье");
+// первый вариант через метод
+int DayWeek(int i)
+{
+if (i > 7 || i < 1) Console.WriteLine("Такого дня не существует");
+if (i == 1) Console.WriteLine("Понедельник");
+if (i == 2) Console.WriteLine("Вторник");
+if (i == 3) Console.WriteLine("Среда");
+if (i == 4) Console.WriteLine("Четверг");
+if (i == 5) Console.WriteLine("Пятница");
+if (i == 6) Console.WriteLine("Суббота");
+if (i == 7) Console.WriteLine("Воскресенье");
+return i;
+}
+
+DayWeek(i);
+
+// вариант через switch/case
 
 switch (i)
 {
