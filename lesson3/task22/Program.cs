7a20cc5e46e470513f52a254ad02255fb0118892
VanUliK <vanulik@work.com>
2022-08-29 14:20:14 +0200

    reload task22

--- lesson3/task22/Program.cs
+++ lesson3/task22/Program.cs
@@ -5,7 +5,17 @@
 Console.WriteLine("Введите число N");
 int N = Convert.ToInt32(Console.ReadLine());
 
+// Первый вариант
 for (int i = 1; i <= N; i++)
 {
     Console.WriteLine(i * i + " ");
 }
+
+// Второй вариант
+string result = "";
+for (int i = 1; i <= N; i++)
+{
+    double res = Math.Pow(i, 2);
+    result = result + res + " | ";
+}
+Console.WriteLine(result);
