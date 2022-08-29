fe8297c2efc1d5eb02a452dcb16c6006f2d69f40
VanUliK <m.vanulik@gmail.com>
2022-08-29 22:22:51 +0200

    solution lesson8

--- lesson8/task57/Program.cs
+++ lesson8/task57/Program.cs
@@ -56,6 +56,8 @@ for (int i = 0; i < matrix.GetLength(0); i++)
 }
 for (int i = 0; i < array.Length; i++)
 {
-
-    Console.WriteLine($"Колличество {i} = {array[i]}");
+    if (array[i] > 0)
+    {
+        Console.WriteLine($"Количество {i} = {array[i]}");
+    }
 }
