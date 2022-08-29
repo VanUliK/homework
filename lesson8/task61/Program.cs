deb167a130df4431faab2f0e1f8e9f51b6b85b64
VanUliK <m.vanulik@gmail.com>
2022-08-29 22:23:03 +0200

    solution lesson8

---
+++ lesson8/task61/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 61: Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
+Console.Clear();
+
+int i, n, c;
+Console.WriteLine("Введите нужное количество строк треугольника Паскаля:");
+string s = Console.ReadLine();
+n = Convert.ToInt32(s);
+
+int factorial(int n)
+{
+    int i, x = 1;
+    for (i = 1; i <= n; i++)
+    {
+        x *= i;
+    }
+    return x;
+}
+
+for (i = 0; i < n; i++)
+{
+    for (c = 0; c <= (n - i); c++) // создаём после каждой строки n-i отступов от левой стороны консоли, чем ниже строка, тем меньше отступ
+    {
+        Console.Write(" ");
+    }
+    for (c = 0; c <= i; c++)
+    {
+        Console.Write("  "); // создаём пробелы между элементами треугольника
+        Console.Write(factorial(i) / (factorial(c) * factorial(i - c))); //формула вычисления элементов треугольника
+    }
+    //onsole.WriteLine();
+    Console.WriteLine(); // после каждой строки с числами отступаем две пустые строчки
+}
+Console.ReadLine();
+
---
+++ lesson8/temp/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = -10, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SwapFirstAndLastRows(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+int[,] TransposeMatrix(int[,] matrix)
+{
+    int[,] newMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            newMatrix[i, j] = matrix[j, i];
+        }
+    }
+    return newMatrix;
+}
+
+
+
+Console.Write("Введите m и n через Enter "); 
+int m = Convert.ToInt32(Console.ReadLine());//число строк
+int n = Convert.ToInt32(Console.ReadLine());//число столбцов
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[,] tmatrix = TransposeMatrix(matrix);
+PrintMatrix(tmatrix);
