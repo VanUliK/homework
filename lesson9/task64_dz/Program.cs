c58e4384460f5c4a2228c4cb464ffd23e79449d7
VanUliK <m.vanulik@gmail.com>
2022-09-01 21:35:02 +0200

    solution task64

--- lesson9/task64_dz/Program.cs
+++ lesson9/task64_dz/Program.cs
@@ -5,3 +5,15 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
 
+
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbersToN(number);
+
+void PrintNumbersToN(int n)
+{
+    if (n < 1) return;
+    Console.Write(n + " ");
+    PrintNumbersToN(n - 1);
+}
