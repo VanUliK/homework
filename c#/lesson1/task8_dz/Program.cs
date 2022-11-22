741e1221076ab0f26cd28de479ebc0b4407e3be0
VanUliK <m.vanulik@gmail.com>
2022-11-22 20:55:58 +0200

    reload

--- c#/lesson1/task8_dz/Program.cs
+++ c#/lesson1/task8_dz/Program.cs
@@ -11,3 +11,22 @@ while (counter <= N)
     Console.Write(counter + " ");
     counter = counter + 2;
 }
+
+// Console.Write("Введите 1е число > ");                  // Вывод приглашения ввода
+// string inputnum_A = Console.ReadLine();                // Ввод строки с терминала (консоли)
+// int num_A = Convert.ToInt32(inputnum_A);               // Преобразование строки в число
+// Console.WriteLine("Чётные числа от 1 до " + num_A);
+// int i = 2;
+// bool not = true;
+// while (i <= num_A)
+// {
+
+//     Console.Write(i + ", ");
+//     not = false;
+//     i += 2;
+// }
+
+// if (not)
+// {
+//     Console.WriteLine("Нет чётных чисел!");
+// }
